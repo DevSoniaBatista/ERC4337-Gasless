4ad81a9b215e600de49a8d7c773303f32b9cf844
Afonsodalvi <Afonsodalvi>
2023-06-06 16:52:58 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, localWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={activeChain}
+    supportedWallets={[
+      smartWallet({
+        factoryAddress: "0x129d0fa421d5d10dA2d3B3d622818234f78113cF",
+        thirdwebApiKey: "coloque a sua",
+        gasless: true,
+        personalWallets:[
+          metamaskWallet(),
+          localWallet()
+        ]
+      })
+    ]}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
